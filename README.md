00ea1c55e2f3ece180d426ae4f62d6bd555e4b35
null <gh@nulldaemon.eu.org>
2025-03-12 20:08:13 +0700

    Installing more packages and fixing inet issues

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A Polygram images-building repository.
 ## Deployment
 
 To deploy this project, create a commit with any tag.
+
+## Running
+
+A version of Polygram is required to run this.
--- debian/build-image.sh
+++ debian/build-image.sh
@@ -13,6 +13,8 @@ debootstrap bookworm /mnt http://deb.debian.org/debian || exit 1
 
 echo "/dev/ubd0   ext4    discard,errors=remount-ro  0       1" | tee /mnt/etc/fstab
 cp ../files/polygram-init-network /mnt/usr/bin
+echo "nameserver 10.0.2.3" >/mnt/etc/resolv.conf
+echo "polygram" >/mnt/etc/hostname
 chmod u+x /mnt/usr/bin/polygram-init-network
 cp ../files/polygram-network.service /mnt/etc/systemd/system
 chroot /mnt systemctl enable polygram-network
