350986ff830aa73e98ac6dc2f0138728f8db4016
null <gh@nulldaemon.eu.org>
2025-03-12 20:24:21 +0700

    Fixing internet (2) chore

--- debian/build-image.sh
+++ debian/build-image.sh
@@ -13,13 +13,12 @@ debootstrap bookworm /mnt http://deb.debian.org/debian || exit 1
 
 echo "/dev/ubd0   ext4    discard,errors=remount-ro  0       1" | tee /mnt/etc/fstab
 cp ../files/polygram-init-network /mnt/usr/bin
-echo "nameserver 10.0.2.3" >/mnt/etc/resolv.conf
-echo "polygram" >/mnt/etc/hostname
 chmod u+x /mnt/usr/bin/polygram-init-network
 cp ../files/polygram-network.service /mnt/etc/systemd/system
 chroot /mnt /bin/sh -c "apt update && apt install neofetch -y --no-install-recommends && apt install dbus dbus-x11"
 chroot /mnt systemctl enable polygram-network
 chroot /mnt /bin/sh -c "echo 'root:$ROOT_PASS' | chpasswd"
+echo "polygram" >/mnt/etc/hostname
 umount /mnt
 
 tar -czvf "$FILENAME.tar.gz" "$FILENAME"
